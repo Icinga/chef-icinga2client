0160e15fb03d50661b1568f3e4cbbfb8be0203c8
Virender Khatri <vir.khatri@gmail.com>
2017-11-23 02:27:19 +0530

    added icinga2repo cookbook version

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/Icinga/chef-icinga2client/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/Icinga/chef-icinga2client' if respond_to?(:source_url)
 
-depends 'icinga2repo'
+depends 'icinga2repo', '>= 1.0.0'
 
 %w[fedora redhat centos amazon ubuntu debian raspbian].each do |os|
   supports os
